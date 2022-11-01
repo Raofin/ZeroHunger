34def70fef637ae80621ce4adade3e7bf713c62d
Raofin <raofin@hotmail.com>
2022-11-02 01:58:52 +0600

    Add employees crud

--- ZeroHunger/Controllers/HomeController.cs
+++ ZeroHunger/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZeroHunger.Database;
 
 namespace ZeroHunger.Controllers
 {
@@ -13,18 +14,75 @@ namespace ZeroHunger.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Employees()
         {
-            ViewBag.Message = "Your application description page.";
+            var db = new ZeroHungerEntities();
+            return View(db.Employees.ToList());
+        }
 
+        [HttpGet]
+        public ActionResult AddEmployees()
+        {
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult AddEmployees(Employee employee)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var db = new ZeroHungerEntities();
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction("Employees", "Home");
         }
+
+        [HttpGet]
+        public ActionResult DeleteEmployee(int id)
+        {
+            var employee = new Employee() { Id = id };
+
+            using (var context = new ZeroHungerEntities())
+            {
+                context.Employees.Attach(employee);
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Employees", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateEmployee(int id)
+        {
+            var db = new ZeroHungerEntities();
+            var model = (from employee in db.Employees
+                         where employee.Id == id
+                         select employee).SingleOrDefault();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee model)
+        {
+            using (var db = new ZeroHungerEntities())
+            {
+                var employee = db.Employees.SingleOrDefault(e => e.Id == model.Id);
+
+                if (employee != null)
+                {
+                    employee.Name = model.Name;
+                    employee.Email = model.Email;
+                    employee.Age = model.Age;
+                    employee.Sex = model.Sex;
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Employees", "Home");
+        }
+
+
+
+
     }
 }
